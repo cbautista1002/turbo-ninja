e0d7b9f5fe64f2e2ad42c7e4a1d492143fc50016
Carlos Bautista <cbautista@ise.com>
2015-02-11 00:12:44 +0000

    Added gulp-jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
+// Import necessary modules
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    jshint = require('gulp-jshint');
     //plumber = require('gulp-plumber');
 
+// Instead of plumber, simply log error to console
 function errorLog(error){
     console.log(error);
     this.emit('end');
 }
 
-// Minify JS Scripts
+// Minify JS Scripts - Compress and concat JS files
 gulp.task('js_minify', function(){
     gulp.src('js/*.js')
+        .pipe(jshint())
         .pipe(uglify())
         .pipe(concat('min.js'))
         .on('error', errorLog)
         .pipe(gulp.dest('build/js'));
 });
 
-// Watch changes in JS and CSS files
+// Watch changes in JS and CSS files and run tasks on save
 gulp.task('watch', function(){
     gulp.watch('js/**/*.js', ['js_minify']);
 });
